ce3afe9571b0501ab5440e4b3d52bf586a00ab63
damirka <damirka.ru@gmail.com>
2023-03-31 12:32:05 +0300

    waitForEffectsCert

--- publish.js
+++ publish.js
@@ -9,7 +9,9 @@ if (!process.env.MNEMONIC) {
 }
 
 const keypair = sdk.Ed25519Keypair.deriveKeypair(process.env.MNEMONIC);
-const provider = new sdk.JsonRpcProvider(new sdk.Connection({ fullnode: 'https://fullnode.testnet.sui.io/' }));
+const provider = new sdk.JsonRpcProvider(
+  new sdk.Connection({ fullnode: "https://fullnode.testnet.sui.io/" })
+);
 const signer = new sdk.RawSigner(keypair, provider);
 
 const compiledModulesAndDeps = JSON.parse(
@@ -31,35 +33,33 @@ const compiledModulesAndDeps = JSON.parse(
 
     tx.transferObjects([upgradeCap], tx.pure(await signer.getAddress()));
 
-    const result = await signer.signAndExecuteTransactionBlock(
-      {
-        transactionBlock: tx,
-        options: {
-          showEffects: true,
-          showObjectChanges: true,
-          showEvents: true,
-        },
+    const result = await signer.signAndExecuteTransactionBlock({
+      transactionBlock: tx,
+      options: {
+        showEffects: true,
+        showObjectChanges: true,
+        showEvents: true,
       },
-      "WaitForLocalExecution"
-    );
-
-    // console.log(JSON.stringify(result, null, 4));
+    });
 
     let chg = result.objectChanges;
-    let item = chg.find((r) => r.type == "created" && r.objectType.includes("Item"));
+    let item = chg.find(
+      (r) => r.type == "created" && r.objectType.includes("Item")
+    );
 
     return [
       chg.find((r) => r.type == "published").packageId,
-      chg.find((r) => r.type == "created" && r.objectType.includes("Publisher")).objectId,
+      chg.find((r) => r.type == "created" && r.objectType.includes("Publisher"))
+        .objectId,
       item.objectId,
-      item.objectType
+      item.objectType,
     ];
   })();
 
   // wait for some time - fn needs to fetch coin changes
   await require("util").promisify(setTimeout)(10000);
 
-  console.log('create a kiosk, policy and place an item');
+  console.log("create a kiosk, policy and place an item");
 
   const [kiosk, policy] = await (async function kiosk() {
     const tx = new sdk.TransactionBlock();
@@ -67,108 +67,103 @@ const compiledModulesAndDeps = JSON.parse(
     tx.moveCall({
       target: `0x2::kiosk::place_and_list`,
       typeArguments: [itemType],
-      arguments: [
-        kiosk,
-        kiosk_cap,
-        tx.object(item),
-        tx.pure(100000, 'u64')
-      ]
+      arguments: [kiosk, kiosk_cap, tx.object(item), tx.pure(100000, "u64")],
     });
 
     let [policy, policy_cap] = tx.moveCall({
       target: `0x2::transfer_policy::new`,
       typeArguments: [itemType],
-      arguments: [ tx.object(publisher) ]
+      arguments: [tx.object(publisher)],
     });
 
-    tx.transferObjects([policy_cap, kiosk_cap], tx.pure(await signer.getAddress(), 'address'));
+    tx.transferObjects(
+      [policy_cap, kiosk_cap],
+      tx.pure(await signer.getAddress(), "address")
+    );
+
     tx.moveCall({
       target: `0x2::transfer::public_share_object`,
       typeArguments: [`0x2::kiosk::Kiosk`],
-      arguments: [ kiosk ]
+      arguments: [kiosk],
     });
 
     tx.moveCall({
       target: `0x2::transfer::public_share_object`,
       typeArguments: [`0x2::transfer_policy::TransferPolicy<${itemType}>`],
-      arguments: [ policy ]
+      arguments: [policy],
     });
 
-    const result = await signer.signAndExecuteTransactionBlock(
-      {
-        transactionBlock: tx,
-        options: {
-          showEffects: true,
-          showObjectChanges: true,
-          showEvents: true,
-        },
+    const result = await signer.signAndExecuteTransactionBlock({
+      transactionBlock: tx,
+      options: {
+        showEffects: true,
+        showObjectChanges: true,
+        showEvents: true,
       },
-      "WaitForLocalExecution"
-    );
+    });
 
-    // console.log(JSON.stringify(result, null, 4));
     let chg = result.objectChanges;
-
-    let kiosk_obj = chg.find((r) => r.type === "created" && r.objectType === "0x2::kiosk::Kiosk");
-    let policy_obj = chg.find((r) => r.type === "created" && r.objectType.includes("TransferPolicy<"));
+    let kiosk_obj = chg.find(
+      (r) => r.type === "created" && r.objectType === "0x2::kiosk::Kiosk"
+    );
+    let policy_obj = chg.find(
+      (r) => r.type === "created" && r.objectType.includes("TransferPolicy<")
+    );
 
     return [
       {
         objectId: kiosk_obj.objectId,
         initialSharedVersion: kiosk_obj.version,
-        mutable: true
+        mutable: true,
       },
       {
         objectId: policy_obj.objectId,
         initialSharedVersion: policy_obj.version,
         mutable: false,
-      }
+      },
     ];
   })();
 
-  console.log('purchase an item and deal with tranfer request');
+  console.log("purchase an item and deal with tranfer request");
 
-  console.log(' - Item: %s', item);
-  console.log(' - Item type: %s', itemType);
-  console.log(' - Kiosk: %s', kiosk);
-  console.log(' - TransferPolicy: %s', policy);
+  console.log(" - Item: %s", item);
+  console.log(" - Item type: %s", itemType);
+  console.log(" - Kiosk: %s", kiosk);
+  console.log(" - TransferPolicy: %s", policy);
 
   await (async function () {
     const tx = new sdk.TransactionBlock();
-    let payment = tx.splitCoins(tx.gas, [tx.pure(100000, 'u64')]);
+    let payment = tx.splitCoins(tx.gas, [tx.pure(100000, "u64")]);
     const [purchased_item, request] = tx.moveCall({
       target: `0x2::kiosk::purchase`,
       typeArguments: [itemType],
       arguments: [
         tx.object({ Object: { Shared: kiosk } }),
-        tx.pure(item, 'address'),
-        payment
-      ]
+        tx.pure(item, "address"),
+        payment,
+      ],
     });
 
-    tx.transferObjects([purchased_item], tx.pure(await signer.getAddress(), 'address'));
+    tx.transferObjects(
+      [purchased_item],
+      tx.pure(await signer.getAddress(), "address")
+    );
     tx.moveCall({
       target: `0x2::transfer_policy::confirm_request`,
       typeArguments: [itemType],
-      arguments: [
-        tx.object({ Object: { Shared: policy } }),
-        request
-      ]
+      arguments: [tx.object({ Object: { Shared: policy } }), request],
     });
 
-    const result = await signer.signAndExecuteTransactionBlock(
-      {
-        transactionBlock: tx,
-        options: {
-          showEffects: true,
-          showObjectChanges: true,
-          showEvents: true,
-        },
+    const result = await signer.signAndExecuteTransactionBlock({
+      transactionBlock: tx,
+      options: {
+        showEffects: true,
+        showObjectChanges: true,
+        showEvents: true,
       },
-      "WaitForLocalExecution"
-    );
+    });
 
     console.log(JSON.stringify(result, null, 4));
-    console.log('ADDRESS IS: %s', await signer.getAddress());
+    console.log("ADDRESS IS: %s", await signer.getAddress());
   })();
 })();
